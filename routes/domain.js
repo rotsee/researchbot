6942ae89d1646cfc0f518fec79c0609bbb918bab
rotsee <leo_wallentin@hotmail.com>
2016-06-20 16:41:37 +0100

    structure

--- routes/domain.js
+++ routes/domain.js
@@ -1,11 +1,19 @@
+var whois = require('node-whois')
 module.exports = function() {
 
   var api = {}
 
   api.get = function(string, callback) {
-    var whois = require('node-whois')
     whois.lookup(string, function(err, data) {
-      callback(null, data)
+      output = {}
+      var rows = data.split("\r\n")
+      rows.forEach(function(row){
+        var parts = row.split(": ")
+        if (parts.length == 2){
+          output[parts[0].trim()] = parts[1].trim()
+        }
+      })
+      callback(null, output)
     })
   }
 
