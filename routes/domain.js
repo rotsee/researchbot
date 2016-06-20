aa565a2bff847eb36666ff7c911d674c6abc3b58
rotsee <leo_wallentin@hotmail.com>
2016-06-20 18:28:49 +0100

    use different algorithms

--- routes/domain.js
+++ routes/domain.js
@@ -1,19 +1,75 @@
-var whois = require('node-whois')
+var whois = require("node-whois")
+var async = require("async")
+var keys = {
+  person: [
+    "Registrant Name",
+    "Admin Name",
+
+  ],
+  organization: [
+    "そしきめい",
+    "組織名",
+    "Organization",
+    "Registrant Organization",
+    "Admin Organization"
+  ]
+}
+
+var splitters = [
+  function byColon(data, cb){
+  // Split by newline and then by colon
+  // This is probably the most common format
+    output = {}
+    var rows = data.split("\r\n")
+    rows.forEach(function(row){
+      var parts = row.split(":")
+      if (parts.length > 1){
+        output[parts[0].replace(/>/g,"").trim()] = parts[1].replace(/</g,"").trim()
+        // Also remove >>> and <<< used by some whois servers
+      }
+    })
+    return cb(null, output)
+  },
+  function bySpaces(data, cb){
+  //a. [Domain Name]                YOMIURI.CO.JP
+    output = {}
+    var rows = data.split("\r\n")
+    rows.forEach(function(row){
+      var parts = row.split(/[\s\t]{2,50}/)
+      if (parts.length > 1){
+        output[parts[0].replace(/\w\.\s/, "").replace("]", "").replace("[", "").trim()] = parts[1].trim()
+      }
+    })
+    return cb(null, output)    
+  }
+]
+
 module.exports = function() {
 
   var api = {}
 
   api.get = function(string, callback) {
     whois.lookup(string, function(err, data) {
-      output = {}
-      var rows = data.split("\r\n")
-      rows.forEach(function(row){
-        var parts = row.split(": ")
-        if (parts.length == 2){
-          output[parts[0].replace(/>/g,"").trim()] = parts[1].replace(/</g,"").trim()
+      async.applyEach(splitters, data, function(err, whoisoutputs){
+        whoisdict = {
+          person: [],
+          organization: []
         }
+        whoisoutputs.forEach(function (whoisoutput){
+          for (var category in keys){
+            for (var key in whoisoutput){
+              if (keys[category].indexOf(key) > -1 ){
+                if (whoisdict[category].indexOf(whoisoutput[key]) == -1){
+                  whoisdict[category].push(whoisoutput[key])
+                }
+              }
+            }
+          }
+        })
+        callback(null,
+          {whois: whoisdict}
+        )
       })
-      callback(null, output)
     })
   }
 
