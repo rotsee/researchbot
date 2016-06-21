0e7ba7e08d462b56d0dfe427cc5658c66dca5c62
Ana Isabel Carvalho <a@manufacturaindependente.org>
2016-06-21 16:31:13 +0100

    Added more detail for other TLD

--- routes/topdomain_messages.js
+++ routes/topdomain_messages.js
@@ -5,16 +5,21 @@ module.exports = {
     "es": "`.es` domains can only be checked from http://www.nic.es/.",
     "gr": "`.gr` domains can only be checked from https://grweb.ics.forth.gr/public/whois.jsp?lang=en",
     "fr": "`.fr` domains registred by private persons will have some data hidden here. You might be able to get a more detailed answer at http://www.afnic.fr/.",
-    "ar": "`.ar` domains can only be checked at http://www.nic.er. To make the request enter the domain in Buscar Dominios",
+    "ar": "`.ar` domains can only be checked at http://www.nic.ar. To make the request enter the domain in Buscar Dominios",
     "au": "`.au` domains can be checked at http://whois.ausregistry.com.au for more information",
     "gh": "`.gh` domains can only be checked at http://www.nic.gh/whois.html",
     "ax": "`.ax` domains can only be checked at http://.whois.ax/index.php?l=en",
+    "az": "`.az` domains have no whois server but you can check whois at http://nic.az",
+    "ba": "`.ba` domains have no whois server but you can check whois at http://www.nic.ba/stream/whois",
+    "bb": "`.bb` domains have no whois server but you can check whois at http://whois.telecoms.gov.bb/search_domain.php",
+    "bd": "`.bd` domains have no whois server but you can check whois at http://whois.btcl.net.bd",
+    "be": "`.be` domains can only be checked at http://www.dnsbelgium.be/en. Enter the URL you would like to get information about on the search box for _Is your preferred domain name still available?_ You must agree to the terms and conditions of the service."
+    
     
     "mil": "`.mil` domains have no whois server.",
     "ad": "`.ad` domains have no whois server.",
     "al": "`.al` domains have no whois server.",
     "ao": "`.ao` domains have no whois server.",    
     "aq": "`.aq` domains have no whois server.",
-    "az": "`.az` domains have no whois server.",
     
 }
